c384d98d727cba14baaf2b046c9039cabd821f08
Alex Nakos <alex.nakos9@gmail.com>
2019-05-28 19:24:43 -0500

    Added completion status field, will need to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_19_182312) do
+ActiveRecord::Schema.define(version: 2019_05_25_023432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_19_182312) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "completion_status", default: 0
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
