aa6f31536c32f5278dd8c3ace2582ac4fa7ddb0f
Brandon <Brandkessinger@gmail.com>
2019-06-12 08:52:56 -0500

    Root is now login page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :tasks
   resources :projects
   resources :users
-  root 'home#index'
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # User related routes
