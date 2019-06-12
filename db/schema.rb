dcd7ffa9471f973309647ee55b049f0a872523fe
Alex Nakos <alex.nakos9@gmail.com>
2019-06-12 13:27:03 -0500

    Added posts to our DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_204208) do
+ActiveRecord::Schema.define(version: 2019_06_12_181531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "details", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
