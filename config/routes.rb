201b780b8c188ed12a310b498de2123b0d564170
Brandon <Brandkessinger@gmail.com>
2019-06-12 09:55:59 -0500

    root fix for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   resources :users
 
   devise_scope :user do
-    root 'devise/sessions#new'
+    authenticated :user do
+      root 'details#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
